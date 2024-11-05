2cac2dbb24521db6a9ecb4b5b53b74baca180909
Abhijith-Abi <abhijithabhijith1999@gmail.com>
2024-11-05 12:38:59 +0530

    add all packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 export default function Home() {
     return (
         <div>
             <Button variant="secondary">Click Me</Button>
+            <Textarea placeholder="Type your message here." />
         </div>
     );
 }
