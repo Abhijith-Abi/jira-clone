639a6048d2deecf48432510e42e8a2c25e491601
Abhijith-Abi <abhijithabhijith1999@gmail.com>
2024-11-05 14:45:53 +0530

    fix button components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
-import Image from "next/image";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
     return (
-        <div>
-            <Button variant="secondary">Click Me</Button>
-            <Textarea placeholder="Type your message here." />
+        <div className="gap-4 flex p-4 h-dvh">
+            <Input />
+            <Button>Primary</Button>
+            <Button variant="secondary">Secondary</Button>
+            <Button variant="destructive">Destructive</Button>
+            <Button variant="ghost">Ghost</Button>
+            <Button variant="muted">Link</Button>
+            <Button variant="outline">Outline</Button>
+            <Button variant="teritary">Teritary</Button>
         </div>
     );
 }
